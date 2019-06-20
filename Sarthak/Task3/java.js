85c191ace2871bb0c15436af3cb03faa7a6ac486
sarthakme <46780302+sarthakme@users.noreply.github.com>
2019-06-20 23:53:27 +0530

    Task 3 incomplete
    I have not added the timer or the css formatting

---
+++ Sarthak/Task3/java.js
@@ -0,0 +1,95 @@
+var x=0;
+var flag=0;
+function score() {
+    x=0;
+    if(document.getElementById('2').checked) {
+        x=x+1;
+    }
+    if(document.getElementById('5').checked) {
+        x=x+1;
+    }
+    if(document.getElementById('12').checked) {
+        x=x+1;
+    }
+    if(document.getElementById('15').checked) {
+        x=x+1;
+    }
+    if(document.getElementById('17').checked) {
+        x=x+1;
+    }
+    sessionStorage.setItem("score",x);
+}
+
+function display() {
+    document.getElementById('h1').innerHTML = "Your score is " + sessionStorage.getItem('score') + "/5";
+}
+
+function quest2() {
+    if(document.getElementById('1').checked||document.getElementById('2').checked||document.getElementById('3').checked||document.getElementById('4').checked) {
+        flag=1;
+    }
+    if(flag==0) {
+        alert('No option has been selected. Are you sure you want to continue? You cannot attempt this question after skipping it')
+        flag=1;
+    }
+    else {
+        document.getElementById('first').style.display = 'none';
+        document.getElementById('second').style.display = 'block';
+        flag=0;
+    }
+}
+
+function quest3() {
+    if(document.getElementById('5').checked||document.getElementById('6').checked||document.getElementById('7').checked||document.getElementById('8').checked) {
+        flag=1;
+    }
+    if(flag==0) {
+        alert('No option has been selected. Are you sure you want to continue? You cannot attempt this question after skipping it')
+        flag=1;
+    }
+    else {
+        document.getElementById('second').style.display = 'none';
+        document.getElementById('third').style.display = 'block';
+        flag=0;
+    }
+}
+
+function quest4() {
+    if(document.getElementById('9').checked||document.getElementById('10').checked||document.getElementById('11').checked||document.getElementById('12').checked) {
+        flag=1;
+    }
+    if(flag==0) {
+        alert('No option has been selected. Are you sure you want to continue? You cannot attempt this question after skipping it')
+        flag=1;
+    }
+    else {
+        document.getElementById('third').style.display = 'none';
+        document.getElementById('fourth').style.display = 'block';
+        flag=0;
+    }
+}
+
+function quest5() {
+    if(document.getElementById('13').checked||document.getElementById('14').checked||document.getElementById('15').checked||document.getElementById('16').checked) {
+        flag=1;
+    }
+    if(flag==0) {
+        alert('No option has been selected. Are you sure you want to continue? You cannot attempt this question after skipping it')
+        flag=1; 
+    }
+    else {
+        document.getElementById('fourth').style.display = 'none';
+        document.getElementById('fifth').style.display = 'block';
+        flag=0;
+    }
+}
+
+function finish() {
+    if(document.getElementById('17').checked||document.getElementById('18').checked||document.getElementById('19').checked||document.getElementById('20').checked) {
+        flag=1;
+    }
+    if(flag==0) {
+        alert('No option has been selected. Are you sure you want to continue? You cannot attempt this question after skipping it')
+        flag=1; 
+    }
+}
